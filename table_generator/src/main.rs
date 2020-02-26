d385919a8e3f5d2605c5e029c568ce47847939ff
Hasnain Ali <beinghasnain16@gmail.com>
2020-02-26 18:23:04 +0500

    Created a simple multiplication table generator

---
+++ table_generator/src/main.rs
@@ -0,0 +1,18 @@
+use std::io;
+use std::io::Write;
+fn main() {
+    println!("Welcome to Multiplication Table Generator");
+    print!("Enter range of table: ");
+    io::stdout().flush().unwrap();
+    let mut r = String::new();
+    io::stdin().read_line(&mut r).unwrap();
+    let y: i32 = r.trim().parse().unwrap();
+    print!("Enter a number: ");
+    io::stdout().flush().unwrap();
+    let mut n = String::new();
+    io::stdin().read_line(&mut n).unwrap();
+    let x: i32 = n.trim().parse().unwrap();
+    for i in 1..y+1{
+        println!("{} x {} = {}",x,i,x*i);
+    }
+}
